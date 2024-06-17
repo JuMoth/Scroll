956d2080ee629f3117fe60bd8c5d9a81647e598b
JuMoth <tkdlqjqkd@gmail.com>
2024-06-17 16:14:39 +0900

    Feat: Scroll Practice

--- output.js
+++ output.js
@@ -1,7 +1,21 @@
 (function() {
     const outputElem = document.querySelector('.output');
+    const ilbuniElem = document.querySelector('.ilbuni');
+
+    function showValue() {
+        // outputElem.innerHTML = window.scrollY;
+        // outputElem.innerHTML = ilbuniElem.offsetTop;
+        let posY = ilbuniElem.getBoundingClientRect().top;
+        outputElem.innerHTML = posY;
+
+        if(posY < window.innerHeight * 0.2) {
+            ilbuniElem.classList.add('zoom');
+        } else {
+            ilbuniElem.classList.remove('zoom');
+        }
+    }
 
     window.addEventListener('scroll', () => {
-        outputElem.innerHTML = window.pageYOffset;
+        showValue();
     })
 })();
