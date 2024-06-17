183dccc8b4a54bc3e78ad9357ab3efefd4279b21
JuMoth <tkdlqjqkd@gmail.com>
2024-06-17 15:31:02 +0900

    first commit

---
+++ output.js
@@ -0,0 +1,7 @@
+(function() {
+    const outputElem = document.querySelector('.output');
+
+    window.addEventListener('scroll', () => {
+        outputElem.innerHTML = window.pageYOffset;
+    })
+})();
